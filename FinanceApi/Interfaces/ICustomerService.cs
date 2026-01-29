f2c59b8023ff688b2b09632fbd8954a8a85389f3
venumuthusamy <venumuthusamy@gmail.com>
2026-01-29 09:27:24 +0530

    pp

--- FinanceApi/Interfaces/ICustomerService.cs
+++ FinanceApi/Interfaces/ICustomerService.cs
@@ -8,6 +8,6 @@ namespace FinanceApi.Interfaces
         Task<CustomerDto?> GetByIdAsync(int id);
         Task<Customer> CreateAsync(Customer customer);
         Task<Customer?> UpdateAsync(int id, Customer customer);
-        Task<bool> DeleteAsync(int id);
+        Task<bool> DeleteAsync(int id); 
     }
 }
